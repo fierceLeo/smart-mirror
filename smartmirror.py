dbc9b2d91981f259a8c53410017da224e1f68680
fierceLeo <38284241+fierceLeo@users.noreply.github.com>
2018-05-17 20:45:48 +0800

    Update smartmirror.py

--- smartmirror.py
+++ smartmirror.py
@@ -2,7 +2,7 @@
 # requirements
 # requests, feedparser, traceback, Pillow
 
-from Tkinter import *
+from tkinter import *
 import locale
 import threading
 import time
@@ -10,6 +10,12 @@ import requests
 import json
 import traceback
 import feedparser
+
+###########################
+import urllib.request , sys
+import pickle
+# _*_ coding:utf-8 _*_
+###########################
 
 from PIL import Image, ImageTk
 from contextlib import contextmanager
@@ -42,19 +48,49 @@ def setlocale(name): #thread proof function to work with locale
 # maps open weather icons to
 # icon reading is not impacted by the 'lang' parameter
 icon_lookup = {
-    'clear-day': "assets/Sun.png",  # clear sky day
-    'wind': "assets/Wind.png",   #wind
-    'cloudy': "assets/Cloud.png",  # cloudy day
-    'partly-cloudy-day': "assets/PartlySunny.png",  # partly cloudy day
-    'rain': "assets/Rain.png",  # rain day
-    'snow': "assets/Snow.png",  # snow day
-    'snow-thin': "assets/Snow.png",  # sleet day
-    'fog': "assets/Haze.png",  # fog day
-    'clear-night': "assets/Moon.png",  # clear sky night
-    'partly-cloudy-night': "assets/PartlyMoon.png",  # scattered clouds night
-    'thunderstorm': "assets/Storm.png",  # thunderstorm
-    'tornado': "assests/Tornado.png",    # tornado
-    'hail': "assests/Hail.png"  # hail
+    '0': "assets/img/0.png", 
+    '1': "assets/img/1.png",
+    '2': "assets/img/2.png", 
+    '3': "assets/img/3.png",
+    '4': "assets/img/4.png",
+    '5': "assets/img/5.png", 
+    '6': "assets/img/6.png",
+    '7': "assets/img/7.png",
+    '8': "assets/img/8.png", 
+    '9': "assets/img/9.png",
+    '10': "assets/img/10.png",
+    '11': "assets/img/11.png", 
+    '12': "assets/img/12.png",
+    '13': "assets/img/13.png",
+    '14': "assets/img/14.png", 
+    '15': "assets/img/15.png",
+    '16': "assets/img/16.png",
+    '17': "assets/img/17.png", 
+    '18': "assets/img/18.png",
+    '19': "assets/img/19.png", 
+    '20': "assets/img/20.png",
+    '21': "assets/img/21.png", 
+    '22': "assets/img/22.png",
+    '23': "assets/img/23.png",
+    '24': "assets/img/24.png", 
+    '25': "assets/img/25.png",
+    '26': "assets/img/26.png",
+    '27': "assets/img/27.png", 
+    '28': "assets/img/28.png",
+    '29': "assets/img/29.png",
+    '30': "assets/img/30.png", 
+    '31': "assets/img/31.png",
+    '32': "assets/img/32.png",
+    '49': "assets/img/49.png", 
+    '53': "assets/img/53.png",
+    '54': "assets/img/54.png",
+    '55': "assets/img/55.png", 
+    '56': "assets/img/56.png",
+    '57': "assets/img/57.png",
+    '58': "assets/img/58.png",
+    '99': "assets/img/99.png", 
+    '301': "assets/img/301.png",
+    '302': "assets/img/302.png",
 }
 
 
@@ -116,7 +152,7 @@ class Weather(Frame):
         self.iconLbl.pack(side=LEFT, anchor=N, padx=20)
         self.currentlyLbl = Label(self, font=('Helvetica', medium_text_size), fg="white", bg="black")
         self.currentlyLbl.pack(side=TOP, anchor=W)
-        self.forecastLbl = Label(self, font=('Helvetica', small_text_size), fg="white", bg="black")
+        self.forecastLbl = Label(self, font=('Heiti SC', small_text_size), fg="white", bg="black", wraplength=400, justify = 'left')
         self.forecastLbl.pack(side=TOP, anchor=W)
         self.locationLbl = Label(self, font=('Helvetica', small_text_size), fg="white", bg="black")
         self.locationLbl.pack(side=TOP, anchor=W)
@@ -134,6 +170,7 @@ class Weather(Frame):
 
     def get_weather(self):
         try:
+            '''
 
             if latitude is None and longitude is None:
                 # get location
@@ -152,16 +189,37 @@ class Weather(Frame):
                 location2 = ""
                 # get weather
                 weather_req_url = "https://api.darksky.net/forecast/%s/%s,%s?lang=%s&units=%s" % (weather_api_token, latitude, longitude, weather_lang, weather_unit)
+            '''
+            ######################################start############################################
+            host = 'http://jisutqybmf.market.alicloudapi.com'
+            path = '/weather/query'
+            method = 'GET'
+            appcode = 'c297d13e095d48e1aaea6a51ae1bdf86'
+            querys = 'ip=183.207.217.224'
+            bodys = {}
+            url = host + path + '?' + querys
 
-            r = requests.get(weather_req_url)
-            weather_obj = json.loads(r.text)
+            request = urllib.request.Request(url)
+            request.add_header('Authorization', 'APPCODE ' + appcode)
+            response = urllib.request.urlopen(request)
+            content = response.read()
+            weather_obj = json.loads(content)
+            ############################################end###############################################
 
+            #r = requests.get(weather_req_url)
+            #weather_obj = json.loads(r.text)
+
+            #degree_sign= u'\N{DEGREE SIGN}'
+            #temperature2 = "%s%s" % (str(int(weather_obj['currently']['temperature'])), degree_sign)
+            #currently2 = weather_obj['currently']['summary']
+            #forecast2 = weather_obj["hourly"]["summary"]
+            ############################################start##########################################
             degree_sign= u'\N{DEGREE SIGN}'
-            temperature2 = "%s%s" % (str(int(weather_obj['currently']['temperature'])), degree_sign)
-            currently2 = weather_obj['currently']['summary']
-            forecast2 = weather_obj["hourly"]["summary"]
-
-            icon_id = weather_obj['currently']['icon']
+            temperature2 = "%s%s" % (str(int(weather_obj['result']['temp'])), degree_sign)
+            currently2 = weather_obj['result']['city']
+            forecast2 = weather_obj["result"]["index"][1]['detail']
+            ###############################################end#####################################
+            icon_id = weather_obj["result"]["img"]
             icon2 = None
 
             if icon_id in icon_lookup:
@@ -190,6 +248,7 @@ class Weather(Frame):
             if self.temperature != temperature2:
                 self.temperature = temperature2
                 self.temperatureLbl.config(text=temperature2)
+                '''
             if self.location != location2:
                 if location2 == ", ":
                     self.location = "Cannot Pinpoint Location"
@@ -197,9 +256,10 @@ class Weather(Frame):
                 else:
                     self.location = location2
                     self.locationLbl.config(text=location2)
+                '''
         except Exception as e:
             traceback.print_exc()
-            print "Error: %s. Cannot get weather." % e
+            print( "Error: %s. Cannot get weather." % e)
 
         self.after(600000, self.get_weather)
 
@@ -224,19 +284,39 @@ class News(Frame):
             # remove all children
             for widget in self.headlinesContainer.winfo_children():
                 widget.destroy()
+                '''
             if news_country_code == None:
                 headlines_url = "https://news.google.com/news?ned=us&output=rss"
             else:
                 headlines_url = "https://news.google.com/news?ned=%s&output=rss" % news_country_code
 
             feed = feedparser.parse(headlines_url)
+            '''
+            #######################################################################
+            host = 'http://toutiao-ali.juheapi.com'
+            path = '/toutiao/index'
+            method = 'GET'
+            appcode = 'c297d13e095d48e1aaea6a51ae1bdf86'
+            querys = 'type=type'
+            bodys = {}
+            url = host + path + '?' + querys
 
-            for post in feed.entries[0:5]:
-                headline = NewsHeadline(self.headlinesContainer, post.title)
-                headline.pack(side=TOP, anchor=W)
+            request = urllib.request.Request(url)
+            request.add_header('Authorization', 'APPCODE ' + appcode)
+            response = urllib.request.urlopen(request)
+            content = response.read()
+            
+            headlines = json.loads(content)
+            for current_news in range(0, 3):
+                eachHeadlines = (headlines['result']['data'][current_news]['title'])
+                ########################################################################
+
+                #for post in feed.entries[0:5]:
+                headline = NewsHeadline(self.headlinesContainer, eachHeadlines)
+                headline.pack(side=TOP, anchor=W, )
         except Exception as e:
             traceback.print_exc()
-            print "Error: %s. Cannot get news." % e
+            print( "Error: %s. Cannot get news." % e)
 
         self.after(600000, self.get_headlines)
 
@@ -255,7 +335,7 @@ class NewsHeadline(Frame):
         self.iconLbl.pack(side=LEFT, anchor=N)
 
         self.eventName = event_name
-        self.eventNameLbl = Label(self, text=self.eventName, font=('Helvetica', small_text_size), fg="white", bg="black")
+        self.eventNameLbl = Label(self, text=self.eventName, font=('Heiti SC', small_text_size), fg="white", bg="black", wraplength = 600, justify = 'left')
         self.eventNameLbl.pack(side=LEFT, anchor=N)
 
 
@@ -312,8 +392,8 @@ class FullscreenWindow:
         self.news = News(self.bottomFrame)
         self.news.pack(side=LEFT, anchor=S, padx=100, pady=60)
         # calender - removing for now
-        # self.calender = Calendar(self.bottomFrame)
-        # self.calender.pack(side = RIGHT, anchor=S, padx=100, pady=60)
+        self.calender = Calendar(self.bottomFrame)
+        self.calender.pack(side = RIGHT, anchor=S, padx=100, pady=60)
 
     def toggle_fullscreen(self, event=None):
         self.state = not self.state  # Just toggling the boolean
